1bae44f632800385ba663ff6306544d9f1c11887
abhishek <abhishek.mis97@gmail.com>
2020-04-14 21:43:48 +0530

    updated css

--- public/js/index.js
+++ public/js/index.js
@@ -3,18 +3,21 @@ console.log("From index.js")
 
 const weatherForm=document.querySelector('form')
 const inp=document.querySelector('input')
-const out=document.querySelectorAll('input')[1]
+const out=document.querySelector('.output')
+
 weatherForm.addEventListener("submit",(event)=>{
   event.preventDefault()
   console.log(inp.value)
+  out.innerHTML="Loading...."
   fetch("/weather?address="+encodeURIComponent(inp.value)).then((res)=>{
+
     res.json().then((datas)=>{
       if(datas.error){
-        console.log(datas.error)
+        out.innerHTML=datas.error
       }
       else{
         console.log(datas.data)
-        out.value=datas.data
+        out.innerHTML=datas.data
 
       }
     })
